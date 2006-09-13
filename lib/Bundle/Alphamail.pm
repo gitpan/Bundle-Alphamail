f794933c60c69fea9205b70ff325224317992549
Anthony W. Kay <awkay69@yahoo.com>
2006-09-13 20:07:09 +0000

    Import of AWKAY/Bundle-Alphamail-0.02 from CPAN.
    gitpan-cpan-distribution: Bundle-Alphamail
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         AWKAY/Bundle-Alphamail-0.02.tar.gz
    gitpan-cpan-author:       AWKAY
    gitpan-cpan-maturity:     released

--- lib/Bundle/Alphamail.pm
+++ lib/Bundle/Alphamail.pm
@@ -1,34 +1,6 @@
 package Bundle::Alphamail;
 
-use 5.008005;
-use strict;
-use warnings;
-
-require Exporter;
-
-our @ISA = qw(Exporter);
-
-# Items to export into callers namespace by default. Note: do not export
-# names by default without a very good reason. Use EXPORT_OK instead.
-# Do not simply export all your public functions/methods/constants.
-
-# This allows declaration	use Bundle::Alphamail ':all';
-# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
-# will save memory.
-our %EXPORT_TAGS = ( 'all' => [ qw(
-	
-) ] );
-
-our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
-
-our @EXPORT = qw(
-	
-);
-
-our $VERSION = '0.01';
-
-
-# Preloaded methods go here.
+our $VERSION = '0.02';
 
 1;
 __END__
@@ -36,68 +8,73 @@ __END__
 
 =head1 NAME
 
-Bundle::Alphamail - Bundle of modules to satisfy Alphamail's perl dependencies
+Bundle::Alphamail - Perl extension for blah blah blah
 
 =head1 SYNOPSIS
 
- C<perl -MCPAN -e 'install Bundle::Alphamail'>
+C<perl -MCPAN -e 'install Bundle::MyBundle'>
 
 =head1 CONTENTS
 
-Apache2::SiteControl 1.01       - 
-Apache2::AuthCookie 3.04        -
-Apache2::Request 2              -
-MIME::Parser 5.4                -
-MIME::Types 1.16                -
-HTML::Entities 1.29             -
-Log::Log4perl 0.47              -
-Unicode::IMAPUtf7 2             -
-Time::Format 1                  -
-Crypt::CBC 2.14                 -
-Crypt::CAST5 0.04               -
-Text::Aspell 0.04               -
-MIME::Words 5.417               -
-Email::Address 1.8              -
-Email::Date 1                   -
-Unicode::Collate 0.51           -
-Date::Calc 5.4                  -
-Math::BigInt::GMP 1.18          -
-Net::SSH::Perl 1.28             -
-Bundle::HTML::Mason 0.09        -
-HTML::Mason 1.3                 -
+Apache2::AuthCookie 3.1
+Apache2::Request 2.0
+Apache::Session 1.6
+CGI 3.2
+Crypt::CAST5 0.04
+Crypt::CBC 2.14
+Crypt::SSLeay 0.5
+Date::Calc 5.4
+Digest::MD5 2
+Email::Address 1.8
+Email::Date 1.03
+GD::Graph 1.43
+HTML::Mason 1.3
+HTML::Parser 3.45
+HTTP::Cookies 1.39
+Log::Log4perl 0.47
+Mail::Procmailrc 1.09
+Math::BigInt::GMP 1.18
+MIME::Tools 5.4
+MIME::Types 1.16
+mod_perl2 2.0
+Net::SMTP 2.29
+Net::SSH::Perl 1.28
+Test::Harness 2.40
+Test::More 0.45
+Text::Aspell 0.04
+Time::HiRes 1.59
+Unicode::Collate 0.52
+Unicode::String 2.0
+WWW::Mechanize 1.15
 
 =head1 DESCRIPTION
 
-These modules are required by RHEL v4r2 to install Alphamail.
+This bundle referneces all of the perl dependencies required by AlphaMail. You
+may choose to set a prefix for alternate installation to avoid having conflict
+with your package management system. 
 
-=head2 EXPORT
+The recommended way of doing this is:
 
-None by default.
+   cpan> o conf makepl_arg 'PREFIX=/usr/local LIB=/usr/local/lib/alphamail'
+   cpan> install Bundle::Alphamail
 
+The configuration parameter will force the installation of the perl modules in 
+the same location as the Alphamail perl modules, so that no extra
+configuration is necessary.
 
-
-=head1 SEE ALSO
-
-Mention other useful documentation such as the documentation of
-related modules or operating system documentation (such as man pages
-in UNIX), or any relevant external documentation such as RFCs or
-standards.
-
-If you have a mailing list set up for your module, mention it here.
-
-If you have a web site set up for your module, mention it here.
+If you used an alternative prefix for intalling Alphamail itself, then modify
+the LIB= setting to reflect this.
 
 =head1 AUTHOR
 
-Hans Kuhn, E<lt>hak@uoregon.eduE<gt>
+Anthony Kay E<lt>tkay@uoregon.eduE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
 Copyright (C) 2006 by University of Oregon
 
 This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself, either Perl version 5.8.5 or,
+it under the same terms as Perl itself, either Perl version 5.8.6 or,
 at your option, any later version of Perl 5 you may have available.
-
 
 =cut
